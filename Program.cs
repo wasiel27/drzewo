46d8678490928a081a7ebab73826bb84c298201e
wasiel27 <tomek.wasiel@interia.pl>
2014-12-10 14:12:40 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ public class Drzewo
 {
   public static void Main(string[] args)
   {
+    Console.WriteLine("Drzewo z jedną gałęzią wychodzącą z lewej strony rozdrzewia drzewa.");
     Console.WriteLine("Drzewo ścięte z jedną gałęzią wychodzącą z lewej strony rozdrzewia drzewa.");
+
   }
 }
